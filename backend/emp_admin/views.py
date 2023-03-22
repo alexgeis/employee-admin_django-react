9280cb21a49b6e4dcabca6dceaf0e69c2e95900d
Alex Geis <siegxela@gmail.com>
2023-03-22 10:48:13 -0600

    finish backend setup of Django with serializers, viewsets, and updated URL routing

--- backend/emp_admin/views.py
+++ backend/emp_admin/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import EmployeeSerializer
+from .models import Employee
 # Create your views here.
+
+class EmployeeView(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+
+    serializer_class = EmployeeSerializer    
+
